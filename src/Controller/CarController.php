529ff5da05432fe0ca493d28686d3c307280bcd3
Melissa Lesne <melissalesne@orange.fr>
2021-08-01 18:22:14 +0200

    Liste des véhicules

---
+++ src/Controller/CarController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\VoitureRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CarController extends AbstractController
+{
+    /**
+     * @Route("/client/cars", name="cars")
+     */
+    public function index(VoitureRepository $repo): Response
+    {
+        $voitures = $repo->findAll();
+        return $this->render('car/index.html.twig', [
+            "voitures" => $voitures
+        ]);
+    }
+}
