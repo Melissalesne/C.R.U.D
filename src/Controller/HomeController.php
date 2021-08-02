d7e89284f077640c27ef439801a0c2b2ad6a495b
Melissa Lesne <melissalesne@orange.fr>
2021-08-02 16:15:30 +0200

    Gestion des utilisateurs

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class HomeController extends AbstractController
 {
@@ -14,5 +19,28 @@ class HomeController extends AbstractController
     public function index(): Response
     {
         return $this->render('home/index.html.twig');
+    }
+
+    /**
+     * @Route("/inscription", name="inscription")
+     */
+    public function inscription(Request $request, ObjectManager $objectManager, UserPasswordEncoderInterface $encoder)
+    {
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $passwordCrypte = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($passwordCrypte);
+            $user->setRoles("ROLE_USER");
+            $objectManager->persist($user);
+            $objectManager->flush();
+            return $this->redirectToRoute("home");
+        }
+
+        return $this->render('home/inscription.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
