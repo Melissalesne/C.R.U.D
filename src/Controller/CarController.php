0293a49fe11a3294ead62f95d0ce99d57254d67e
Melissa Lesne <melissalesne@orange.fr>
2021-08-02 11:51:30 +0200

    Formulaire de recherche filtre

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\RechercheVoiture;
+use App\Form\RechercheVoitureType;
 use App\Repository\VoitureRepository;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -16,14 +18,20 @@ class CarController extends AbstractController
      */
     public function index(VoitureRepository $repo, PaginatorInterface $paginatorInterface, Request $request): Response
     {
+        $rechercheVoiture = new RechercheVoiture();
+
+        $form = $this->createForm(RechercheVoitureType::class, $rechercheVoiture);
+        $form->handleRequest($request);
+
         $voitures = $paginatorInterface->paginate(
-            $repo->findAllWithPagination(),
+            $repo->findAllWithPagination($rechercheVoiture),
             $request->query->getInt('page', 1), /*page number*/
             6 /*limit per page*/
         );
 
         return $this->render('car/index.html.twig', [
-            "voitures" => $voitures
+            "voitures" => $voitures,
+            "form" => $form->createView()
         ]);
     }
 }
